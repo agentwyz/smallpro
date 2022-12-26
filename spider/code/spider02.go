172ec1ce38da11c2cbd2705499561f5284aea188
phpwyz <2012663060@qq.com>
2022-12-26 14:04:58 +0800

    add spider02.go

---
+++ spider/code/spider02.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	url := ""
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Printf("featch url error:%v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("Error status code: %v", resp.StatusCode)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Printf("read content failed: %v", err)
+		return
+	}
+
+	fmt.Println("body:", string(body))
+}
