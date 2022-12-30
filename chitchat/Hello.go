f5902d0b3ad8dc8bd4cc40f19879e61849653504
agentwyz <2012663060@qq.com>
2022-12-31 00:55:43 +0800

    add Hello.go

--- chitchat/Hello.go
+++ chitchat/Hello.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
-//这个名字通常用来表示在指定事件触发
+//hander这个名字通常用来表示在指定事件触发 负责对事件进行处理的回调函数
+//第一个参数为ResponseWriter接口, 第二个参数则为指向Request结构的指针
+//handler函数会从Request(请求)结构中提取相关信息, 然后创建一个HTTP响应
+//最后再通过ResponseWriter接口将响应返回给客户端
 func handler(writer http.ResponseWriter, request *http.Request) {
-	_, _ = fmt.Fprintf(writer, "Hello World, %s", request.URL.Path[1:])
+	_, _ = fmt.Fprintf(writer, "Hello GOLANG!, %s", request.URL.Path[1:])//提取到的参数
 }
 
 func main() {
+	//第二个参数为函数
 	http.HandleFunc("/", handler)
+
+	//server := &Server{Addr: addr, Handler: handler}
+	//return server.ListenAndServe()
 	http.ListenAndServe(":8080", nil)
 }
