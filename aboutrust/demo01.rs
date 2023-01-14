0597155320448cdd78d299227041ef31e2995a49
agentwyz <2012663060@qq.com>
2023-01-14 22:00:49 +0800

    lean tamplate in go

--- aboutrust/demo01.rs
+++ aboutrust/demo01.rs
@@ -1,15 +1,15 @@
-use std::fs
-
+use std::fs;
+  
 fn main() {
     let url = "https://www.rust-lang.org/";
     let output = "rust.md";
 
     println!("{}", url);
-    let body = reqweet::blocking::get(url).unwrap().text().unwrap();
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
 
     println!("Convering html to markdown...");
     let md = html2md::parse_html(&body);
 
     fs::write(output, md.as_bytes()).unwrap();
-    println!("");
+    println!("Converted markdown has been saved in {}", output);
 }
---
+++ vvb/gin/tamplate.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"html/template"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	//解析模版
+	tmpl, err := template.ParseFiles("./hello.tmpl")
+	//渲染模版
+	if err != nil {
+		fmt.Printf("parse template filed, err: %v", err)
+		return
+	}
+	//渲染模版, 第一个参数是你往哪个地方写, 第二个参数是传递的数据
+	err = tmpl.Execute(w, "hello, wangyangzheng")
+	if err != nil {
+		fmt.Printf("Execute tamplate error %v", err)
+		return
+	}
+}
+
+func main() {
+	//创建解析路径
+	http.HandleFunc("/", sayHello)
+
+	err := http.ListenAndServe(":9090", nil)
+	
+	if err != nil {
+		fmt.Printf("HTTP server start error failed, err %v", err)
+	}
+}
+
