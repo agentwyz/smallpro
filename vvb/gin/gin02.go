9d469b16f44e930bfd55a7358337c524a1e9f7b5
agentwyz <2012663060@qq.com>
2023-01-17 17:00:16 +0800

    do a site

---
+++ vvb/gin/gin02.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	route := gin.Default()
+	
+	route.Static("/font", "./statics/assets/")
+
+	route.LoadHTMLFiles("./index.html")
+	
+	route.GET("/index" ,func (context *gin.Context)  {
+		context.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	route.Run(":9090")
+}
